f8b1d6cd8cb4a052fa344b1dd3ae6dced768e4a9
joe-avalos <jose.b.avalos@gmail.com>
2021-07-02 15:52:52 -0500

    challenge correction

--- challenge.js
+++ challenge.js
@@ -5,7 +5,12 @@ module.exports = function sortCategoriesForInsert(inputJson) {
   const parentMap = new Map();
   inputJson.forEach((item) => {
     if (item.parent_id !== null) {
-      parentMap.set(item.parent_id, item);
+      if (parentMap.has(item.parent_id)) {
+        const prev = parentMap.get(item.parent_id);
+        parentMap.set(item.parent_id, [...prev, item]);
+      } else {
+        parentMap.set(item.parent_id, [item]);
+      }
     } else {
       heads.push(item);
     }
@@ -13,11 +18,19 @@ module.exports = function sortCategoriesForInsert(inputJson) {
 
   heads.forEach((head) => {
     result.push(head);
-    let next = parentMap.get(head.id);
-    while (next !== undefined) {
-      result.push(next);
-      next = parentMap.get(next.id);
-    }
+    let children = parentMap.get(head.id);
+    result.push(...recurseHelper(children, parentMap));
   });
+  return result;
+};
+
+const recurseHelper = (children, parentMap, result = []) => {
+  if (children === undefined) return;
+  for (let i = 0; i < children.length; i++) {
+    let child = children[i];
+    result.push(child);
+    let next = parentMap.get(child.id);
+    recurseHelper(next, parentMap, result);
+  }
   return result;
 };
